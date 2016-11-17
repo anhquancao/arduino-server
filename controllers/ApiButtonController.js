96c394a7bbc9d6e9eacb538a0a8941a33d843746
Cao Anh Quan <aquancva@gmail.com>
2016-11-17 17:44:34 +0700

    remove id

--- controllers/ApiButtonController.js
+++ controllers/ApiButtonController.js
@@ -2,7 +2,7 @@ Button = require('../model/Button');
 
 module.exports = {
     buttons: function (req, res) {
-        Button.find({}, {state: 1, port: 1, name: 1, _id: -1}).exec(function (err, buttons) {
+        Button.find({}, {state: 1, port: 1, name: 1, _id: 0}).exec(function (err, buttons) {
             if (err) throw err;
             res.json({buttons: buttons})
         });
