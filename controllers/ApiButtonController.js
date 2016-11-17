0ccdab860dc5e528c388280472baa8673ce115a2
Cao Anh Quan <aquancva@gmail.com>
2016-11-17 17:39:28 +0700

    only retun state,port,name on api

--- controllers/ApiButtonController.js
+++ controllers/ApiButtonController.js
@@ -2,7 +2,7 @@ Button = require('../model/Button');
 
 module.exports = {
     buttons: function (req, res) {
-        Button.find().exec(function (err, buttons) {
+        Button.find({}, {state: 1, port: 1, name: 1}).exec(function (err, buttons) {
             if (err) throw err;
             res.json({buttons: buttons})
         });
